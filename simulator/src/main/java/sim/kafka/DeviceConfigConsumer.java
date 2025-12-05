7cb02942b61d8f0e83c4ddfc300ad9f9cde9ca1b
dengus14 <ddgusev0318@gmail.com>
2025-12-05 14:07:27 -0600

    fix: latest

--- simulator/src/main/java/sim/kafka/DeviceConfigConsumer.java
+++ simulator/src/main/java/sim/kafka/DeviceConfigConsumer.java
@@ -31,7 +31,7 @@ public class DeviceConfigConsumer {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "simulator-group-config");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         this.consumer = new KafkaConsumer<>(props);
